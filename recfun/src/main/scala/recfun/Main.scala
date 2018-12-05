b684e3bd6036fdc81c732c3bd59df7961c6299e9
ireneusz-marciniak <ireneusz.marciniak@schibsted.com>
2018-12-05 15:39:24 +0000

    recfun

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,7 @@
 package recfun
 
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -11,17 +13,69 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    def isEdge(c: Int, r: Int) = r == 0 || c == 0 || c == r
+
+    if (isEdge(c, r)) return 1
+
+    pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
+
+  /**
+    * Exercise 2
+    */
+  def balanceIfStatement(chars: List[Char]): Boolean = {
+    @tailrec
+    def balanceTail(nestLevel: Int, chars: List[Char]): Boolean =
+      if (nestLevel < 0) false
+      else if (chars.isEmpty) nestLevel == 0
+      else if (chars.head == '(') balanceTail(nestLevel + 1, chars.tail)
+      else if (chars.head == ')') balanceTail(nestLevel - 1, chars.tail)
+      else balanceTail(nestLevel, chars.tail)
+
+    balanceTail(0, chars)
+  }
+
+  def balance(chars: List[Char]): Boolean = {
+
+    @tailrec
+    def balanceTail(nestLevel: Int, chars: List[Char]): Boolean =
+      if (nestLevel < 0) false
+      else chars match {
+        case Nil => nestLevel == 0
+        case '(' :: tail => balanceTail(nestLevel + 1, tail)
+        case ')' :: tail => balanceTail(nestLevel - 1, tail)
+        case _ => balanceTail(nestLevel, chars.tail)
+      }
+
+    balanceTail(0, chars)
+  }
+
+  /**
+    * Exercise 3
+    *
+    * money = 5
+    * coins = 1 2
+    *
+    * take first coin = 1
+    * +1 if money can be changed with just 1
+    * + count if money can be changed with multiplies of 1st coin and any combination of other coins
+    * + count money can be changed with other coins without first coin
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+
+    def countChangeForThatCoinOnly(coin: Int, money: Int): Int = if (money % coin == 0) 1 else 0
+
+    if (money <= 0) return 0
+    if (coins.isEmpty) return 0
+
+    val thatCoin = coins.head
+    val withoutThatCoin = coins.tail
+    val countsForRepeatedCoin = for(remainingMoney <- (money - thatCoin) to 0 by (-thatCoin)) yield countChange(remainingMoney, withoutThatCoin)
+
+    countChangeForThatCoinOnly(thatCoin, money) + countsForRepeatedCoin.sum + countChange(money, withoutThatCoin)
+  }
+
+}
